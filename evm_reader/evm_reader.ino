f5c96b79505de75a497b53d19415dccd0a169772
Joe Mynhier <jwmynhie@imail.iu.edu>
2018-03-02 15:13:44 -0500

    Added code to configure the FDC2114 registers.

--- evm_reader/evm_reader.ino
+++ evm_reader/evm_reader.ino
@@ -15,23 +15,43 @@
 byte reg;
 
 void setup() {
+
   // Set up the I2C bus
   Wire.begin(); // Use default clock speed.
+  Wire.setClock(10000);
 
   // Turn on serial monitor.
   Serial.begin(115200);
+  
+  // Reset the FDC2114
+  // Toggle shutdown mode to reset errors.
+  digitalWrite(16, HIGH);
+  delay(1);
+  digitalWrite(16, LOW);
+  delay(1);
+  // Write 0b0 to the sleep mode enable register to exit power on sleep mode.
+  // Configure any status registers before exiting sleep mode.
+  // Writing 0x801 should preserve the other default values in that register.
+  Wire.beginTransmission(0x2B);
+  Wire.write(0x1A);
+  Wire.write(0x8);
+  Wire.write(0x1);
+  delay(1);
+  Wire.endTransmission();
+  
 
   // Select which sensor to read first.
   reg = 0;
- 
-  // Keep the reader awake and don't interrupt.
-  digitalWrite(15, LOW);
-  digitalWrite(16, LOW);
+
 }
 
 void loop() {
   // Read a sensor and print it.
   word result;
+
+  if (digitalRead(15) == HIGH) {
+    Serial.println("FDC2114 in error state!!");
+  }
 
   // Alternate between INT0 and INT1;
   if (reg == 0) {
@@ -42,7 +62,7 @@ void loop() {
 
   result = read_request(0x2B, reg);
   Serial.print("Read ");
-  Serial.print(result);
+  Serial.print(result, HEX);
   Serial.print(" from ");
   Serial.println(reg);
 
@@ -50,30 +70,37 @@ void loop() {
 
 // Read sensor data from a specific device on the i2c bus.
 word read_request(uint8_t rr_device_addr, byte rr_reg) {  
-  byte rr_temp[2] = {0, 0};
+  byte rr_temp[2] = {0xBA, 0xD1};
   byte rr_cnt = 0;
-
-  //Serial.print(" dev: ");
-  //Serial.println(rr_device_addr, HEX);
-  //Serial.print(" reg: ");
-  //Serial.println(rr_reg, HEX);
 
   // Request a 2 byte read at the specified register.
   // FDC2114 sends MSB then LSB
+  Serial.print("Read request for ");
+  Serial.print(rr_device_addr, HEX);
+  Serial.print(":");
+  Serial.print(rr_reg, HEX);
+
+  // Set register to read from.
   Wire.beginTransmission(rr_device_addr);
   Wire.write(rr_reg);
   delay(100);
   Wire.endTransmission();
   
-  Wire.requestFrom(rr_device_addr, (uint8_t)2);
-
   // Read two bytes.
+  Serial.print(". Request returned ");
+  Serial.print(Wire.requestFrom(rr_device_addr, 2));
+
+  Serial.print(". Got ");
+  
   while (Wire.available()) {
     rr_temp[rr_cnt] = Wire.read();
+    Serial.print(rr_temp[rr_cnt]);
+    Serial.print(" ");
     rr_cnt++;
   }
+  Serial.println("");
 
-  // Concatonate into raw data word.
+  // Concatenate into raw data word.
   byte rr_res = (rr_temp[0] << 8) | (rr_temp[1]);
   return rr_res;
 }
