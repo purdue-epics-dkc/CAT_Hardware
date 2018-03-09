993642f33bb1473bcf792e1c3b75639014ad7176
Joe Mynhier <jwmynhie@imail.iu.edu>
2018-03-09 14:34:13 -0500

    Fixed FDC2114 reads of I2C bus in evm_reader

--- feather/evm_reader/evm_reader.ino
+++ feather/evm_reader/evm_reader.ino
@@ -29,15 +29,19 @@ void setup() {
   delay(1);
   digitalWrite(16, LOW);
   delay(1);
+
+  // Set the DRDY_2INT bit in the STATUS_CONFIG register 0x19.
+  // 0x1 should preserve other bits. May not be needed.
+  configure(0x2B, 0x19, 0, 1);
+
+  // Set the MUX_CONFIG register to ready only Ch0, Ch1 (not for final design).
+  // Write 0xe20f to 0x1B. Turns on autoscan, selects correct channels.
+  configure(0x2B, 0x1B, 0xe2, 0xf);
+  
   // Write 0b0 to the sleep mode enable register to exit power on sleep mode.
   // Configure any status registers before exiting sleep mode.
-  // Writing 0x801 should preserve the other default values in that register.
-  Wire.beginTransmission(0x2B);
-  Wire.write(0x1A);
-  Wire.write(0x8);
-  Wire.write(0x1);
-  delay(1);
-  Wire.endTransmission();
+  // Writing 0x1c01 should preserve the other default values in that register (?).
+  configure(0x2B, 0x1A, 0x1C, 0x1);
   
 
   // Select which sensor to read first.
@@ -49,24 +53,40 @@ void loop() {
   // Read a sensor and print it.
   word result;
 
-  if (digitalRead(15) == HIGH) {
-    Serial.println("FDC2114 in error state!!");
-  }
-
   // Alternate between INT0 and INT1;
   if (reg == 0) {
     reg = 2;
+    Serial.print("\t\t");
   } else {
     reg = 0;
+    Serial.println("");
+    delay(100);
   }
 
-  result = read_request(0x2B, reg);
-  Serial.print("Read ");
-  Serial.print(result, HEX);
-  Serial.print(" from ");
-  Serial.println(reg);
+   result = read_request(0x2B, reg);
 
-}
+   Serial.print(reg);
+   Serial.print(": ");
+   Serial.print(result, HEX);
+
+  // Read the STATUS register.
+  
+  Serial.print("\nStatus val at ");
+  Serial.print(digitalRead(15));
+  Serial.print(": ");
+  Wire.beginTransmission(0x2B);
+  Wire.write(0x18);
+  Wire.endTransmission();
+  delay(100);
+  Wire.requestFrom(0x2B, 2);
+  while (Wire.available()) {
+    Serial.print(Wire.read(), HEX);
+    Serial.print(" ");
+  }  
+  Serial.println("");  
+ 
+} 
+
 
 // Read sensor data from a specific device on the i2c bus.
 word read_request(uint8_t rr_device_addr, byte rr_reg) {  
@@ -75,32 +95,33 @@ word read_request(uint8_t rr_device_addr, byte rr_reg) {
 
   // Request a 2 byte read at the specified register.
   // FDC2114 sends MSB then LSB
-  Serial.print("Read request for ");
-  Serial.print(rr_device_addr, HEX);
-  Serial.print(":");
-  Serial.print(rr_reg, HEX);
 
   // Set register to read from.
   Wire.beginTransmission(rr_device_addr);
   Wire.write(rr_reg);
-  delay(100);
   Wire.endTransmission();
+
+  // DEBUG
+  //delay(100);
   
   // Read two bytes.
-  Serial.print(". Request returned ");
-  Serial.print(Wire.requestFrom(rr_device_addr, 2));
-
-  Serial.print(". Got ");
-  
+  Wire.requestFrom(rr_device_addr, 2);
   while (Wire.available()) {
     rr_temp[rr_cnt] = Wire.read();
-    Serial.print(rr_temp[rr_cnt]);
-    Serial.print(" ");
     rr_cnt++;
   }
-  Serial.println("");
 
   // Concatenate into raw data word.
   byte rr_res = (rr_temp[0] << 8) | (rr_temp[1]);
   return rr_res;
 }
+
+void configure(byte device, byte reg, byte upper, byte lower) {
+  Wire.beginTransmission(device);
+  Wire.write(reg);
+  Wire.write(upper);
+  Wire.write(lower);
+  //delay(1);
+  Wire.endTransmission();
+}
+
